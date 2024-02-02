8d009be598253a25404210a11abe0cda941cfbee
alirezaseif20 <alirezaariye@gmail.com>
2024-02-02 21:27:54 +0330

    feat: add theme switcher (lightTheme & darkTheme)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_course/constants/my_theme.dart';
+import 'package:provider_course/providers/theme_provider.dart';
 import 'package:provider_course/widgets/theme_switcher.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(const MyMatrialApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+        )
+      ],
+      child: const MyMatrialApp(),
+    ),
+  );
 }
 
 class MyMatrialApp extends StatefulWidget {
@@ -19,17 +31,24 @@ class MyMatrialApp extends StatefulWidget {
 class _MyMatrialAppState extends State<MyMatrialApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Directionality(
-        textDirection: TextDirection.ltr,
-        child: Scaffold(
-          appBar: AppBar(
-            actions: const [ThemeSwitcher()],
-            title: const Text('ExchangeBs'),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          themeMode: themeProvider.themeMode,
+          theme: MyThemes.lightTheme,
+          darkTheme: MyThemes.darrkTheme,
+          debugShowCheckedModeBanner: false,
+          home: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Scaffold(
+              appBar: AppBar(
+                actions: const [ThemeSwitcher()],
+                title: const Text('ExchangeBs'),
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/widgets/theme_switcher.dart
+++ lib/widgets/theme_switcher.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_course/providers/theme_provider.dart';
 
 class ThemeSwitcher extends StatelessWidget {
   const ThemeSwitcher({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    var switchIcon = Icon(themeProvider.isDarkMode
+        ? CupertinoIcons.moon_fill
+        : CupertinoIcons.sun_max_fill);
+
     return IconButton(
-      onPressed: () {},
-      icon: const Icon(Icons.wb_sunny),
+      onPressed: () {
+        themeProvider.toggleTheme();
+      },
+      icon: switchIcon,
     );
   }
 }
