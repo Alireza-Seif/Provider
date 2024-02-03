6770c7f3e145042198e5c04437844234cd1d09b9
alirezaseif20 <alirezaariye@gmail.com>
2024-02-03 19:16:10 +0330

    feat: add localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_course/constants/my_theme.dart';
 import 'package:provider_course/providers/theme_provider.dart';
 import 'package:provider_course/widgets/theme_switcher.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,6 +37,17 @@ class _MyMatrialAppState extends State<MyMatrialApp> {
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
         return MaterialApp(
+          locale: const Locale('en', ''),
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate
+          ],
+          supportedLocales: const [
+            Locale('en', ''),
+            Locale('fa', ''),
+          ],
           themeMode: themeProvider.themeMode,
           theme: MyThemes.lightTheme,
           darkTheme: MyThemes.darrkTheme,
@@ -44,6 +58,9 @@ class _MyMatrialAppState extends State<MyMatrialApp> {
               appBar: AppBar(
                 actions: const [ThemeSwitcher()],
                 title: const Text('ExchangeBs'),
+              ),
+              body: Center(
+                child: Text(AppLocalizations.of(context)!.book),
               ),
             ),
           ),
