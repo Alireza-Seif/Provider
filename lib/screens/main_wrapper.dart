552b552bb2ede2f84bebe2f8bf67ad4932cd3fbd
alirezaseif20 <alirezaariye@gmail.com>
2024-02-05 19:00:13 +0330

    feat: add BottomNav

--- lib/screens/main_wrapper.dart
+++ lib/screens/main_wrapper.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider_course/screens/home_page.dart';
 import 'package:provider_course/screens/market_view_page.dart';
 import 'package:provider_course/screens/profile_page.dart';
+import 'package:provider_course/screens/watch_list_page.dart';
+import 'package:provider_course/widgets/bottom_nav.dart';
 
 class MainWrapper extends StatefulWidget {
   const MainWrapper({super.key});
@@ -18,13 +20,17 @@ class _MainWrapperState extends State<MainWrapper> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        child: const Icon(Icons.compare_arrows_outlined),
       ),
+      bottomNavigationBar: BottomNav(controller: _myPage,),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: PageView(
         controller: _myPage,
-        children: [
+        children: const [
           HomePage(),
           MarketViewPage(),
           ProfilePage(),
+          WatchListPage(),
         ],
       ),
     );
