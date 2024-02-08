469aa7f38349137b71895ae0e9e1ca4476a168f5
alirezaseif20 <alirezaariye@gmail.com>
2024-02-08 20:48:39 +0330

    feat: add Dio & models for API call

---
+++ lib/providers/crypto_data_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:provider_course/network/api_provider.dart';
+
+class CryptoDataProvider extends ChangeNotifier{
+  ApiProvider apiProvider = ApiProvider();
+
+  getTopMarketCapData(){
+    
+  }
+}
